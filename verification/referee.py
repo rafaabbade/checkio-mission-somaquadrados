e3f72efb0fab3d7c9db54cb70a4ec8d81e2fbb16
rafaabbade <82163300+rafaabbade@users.noreply.github.com>
2023-03-20 09:28:51 -0300

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,7 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "soma",
+            "python": "soma_quadrados",
             "js": "countConsecutiveSummers"
         },
         cover_code={
